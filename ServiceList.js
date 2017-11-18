8c070e3a022508fb9df961395b80d0f1be6210c8
Damien Sendner <damien.sendner@gmail.com>
2017-11-18 17:23:48 +0000

    use section list

--- ServiceList.js
+++ ServiceList.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import { Text, View, FlatList } from 'react-native';
+import { Text, View, FlatList, SectionList } from 'react-native';
+
+function AlarmLine(alarm) {
+    return <Text style={{color: 'white'}}>{alarm.item.metric} {alarm.item.operator} {alarm.item.threshold}</Text>
+}
 
 function ServiceTile({item}) {
     let bg;
@@ -15,29 +19,8 @@ function ServiceTile({item}) {
             bg = 'green';
     }
 
-    return <Text style={{backgroundColor: bg, color: 'white', padding: 10, fontSize: 16, borderColor: 'white', borderWidth: 1}}>{item.name}</Text>;
-}
-
-function AlarmLine(alarm) {
-    return <Text style={{color: 'white'}}>{alarm.item.metric} {alarm.item.operator} {alarm.item.threshold}</Text>
-}
-
-function UnhealthyServiceTile({item}) {
-    let bg;
-
-    switch(item.state) {
-        case 'ALARM':
-            bg = 'red';
-            break;
-        case 'INSUFFICIENT_DATA':
-            bg = 'yellow';
-            break;
-        default:
-            bg = 'green';
-    }
-
     return (
-        <View style={{backgroundColor: bg, padding: 10, borderColor: 'white', borderWidth: 1}}>
+        <View style={{flex: 2, backgroundColor: bg, padding: 10, borderColor: 'white', borderWidth: 1}}>
             <Text style={{fontSize: 16, color: 'white'}}>{item.name}</Text>
             <FlatList data={item.alarms.filter(a => a.state !== 'OK')} renderItem={AlarmLine} keyExtractor={(item, index) => index}/>
         </View>
@@ -52,6 +35,7 @@ function ServiceList({services}) {
         return 0;
     });
     const unhealthyServices = services.filter(s => s.state !== 'OK');
+    const data = [{title: 'Unhealthy Services', data: unhealthyServices}, {title: 'All services', data: allServices}];
 
     return (
         <View style={{flex: 1}}>
@@ -59,10 +43,10 @@ function ServiceList({services}) {
                 <Text style={{paddingTop: 25, paddingLeft: 20, fontSize: 18, fontWeight: 'bold'}}>Services</Text>
             </View>
             <View style={{flex: 1, backgroundColor: 'steelblue'}}>
-                <Text>Unhealthy services</Text>
-                <FlatList data={unhealthyServices} renderItem={UnhealthyServiceTile}/>
-                <Text>All services</Text>
-                <FlatList data={allServices} renderItem={ServiceTile} keyExtractor={(item, index) => index}/>
+                <SectionList
+                    sections={data}
+                    renderItem={ServiceTile}
+                    renderSectionHeader={({section}) => <Text>{section.title}</Text>} />
             </View>
         </View>
     );
