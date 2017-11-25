7a60d696cc1ceb8671f81d8ad8c12cd7d74e95a3
Damien Sendner <damien.sendner@gmail.com>
2017-11-25 11:54:33 +0000

    added tab navigation between healthy/unhealthy services

--- ServiceList.js
+++ ServiceList.js
@@ -1,5 +1,17 @@
 import React from 'react';
-import { Text, View, FlatList, SectionList } from 'react-native';
+import {
+    Text,
+    View,
+    FlatList,
+    TouchableOpacity
+} from 'react-native';
+import {
+    TabRouter,
+    createNavigator,
+    createNavigationContainer,
+    SafeAreaView,
+    addNavigationHelpers,
+} from 'react-navigation';
 
 function AlarmLine(alarm) {
     return <Text style={{color: 'white'}}>{alarm.item.metric} {alarm.item.operator} {alarm.item.threshold}</Text>
@@ -27,6 +39,65 @@ function ServiceTile({item}) {
     );
 }
 
+function ServicesScreen({ services }) {
+    return (
+        <FlatList data={services} renderItem={ServiceTile} />
+    );
+}
+
+function UnhealthyScreen({ screenProps }) {
+    const unhealthyServices = screenProps.services.filter(s => s.state !== 'OK');
+
+    return ServicesScreen( { services: unhealthyServices } );
+}
+
+function HealthyScreen({ screenProps }) {
+    const healthyServices = screenProps.services.filter(s => s.state === 'OK');
+
+    return ServicesScreen( { services: healthyServices } );
+}
+
+const ServicesTabRouter = TabRouter({
+    Unhealthy: {
+        screen: UnhealthyScreen,
+        path: 'unhealthy'
+    },
+    Healthy: {
+        screen: HealthyScreen,
+        path: 'healthy'
+    }
+}, {
+    initialRouteName: 'Unhealthy'
+});
+
+function ServicesTabBar({ navigation }) {
+    const { routes } = navigation.state;
+    return (
+        <SafeAreaView style={{
+            flexDirection: 'row',
+            height: 48,
+        }}>
+            {routes.map(route => (
+                <TouchableOpacity
+                    onPress={() => navigation.navigate(route.routeName)}
+                    key={route.routeName}
+                    style={{
+                        flex: 1,
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        margin: 4,
+                        borderWidth: 1,
+                        borderColor: '#ddd',
+                        borderRadius: 4,
+                    }}
+                    >
+                    <Text>{route.routeName}</Text>
+                </TouchableOpacity>
+            ))}
+        </SafeAreaView>
+    );
+}
+
 function byName(service1, service2) {
     if(service1.name < service2.name) return -1;
     if(service1.name > service2.name) return 1;
@@ -34,29 +105,30 @@ function byName(service1, service2) {
     return 0;
 }
 
-function isHealthy(service) {
-    return service.state === 'OK';
-}
-
-function ServiceList({services}) {
+function ServicesTabView({ services, router, navigation }) {
+    const { routes, index } = navigation.state;
+    const ActiveScreen = router.getComponentForRouteName(routes[index].routeName);
     const orderedServices = services.sort(byName);
-    const healthyServices = orderedServices.filter(isHealthy);
-    const unhealthyServices = orderedServices.filter(s => !isHealthy(s));
-    const data = [{title: 'Unhealthy Services', data: unhealthyServices}, {title: 'Healthy services', data: healthyServices}];
 
     return (
-        <View style={{flex: 1}}>
+        <SafeAreaView forceInset={{ top: 'always' }}>
             <View style={{height: 60, backgroundColor: 'powderblue'}}>
                 <Text style={{paddingTop: 25, paddingLeft: 20, fontSize: 18, fontWeight: 'bold'}}>Services</Text>
             </View>
-            <View style={{flex: 1, backgroundColor: 'steelblue'}}>
-                <SectionList
-                    sections={data}
-                    renderItem={ServiceTile}
-                    renderSectionHeader={({section}) => <Text>{section.title}</Text>} />
-            </View>
-        </View>
+            <ServicesTabBar navigation={navigation} />
+            <ActiveScreen
+                navigation={addNavigationHelpers({
+                        dispatch: navigation.dispatch,
+                        state: routes[index],
+                })}
+                screenProps={{services: orderedServices}}
+            />
+        </SafeAreaView>
     );
 }
 
-export default ServiceList;
+const ServicesTabs = createNavigationContainer(
+    createNavigator(ServicesTabRouter)(ServicesTabView)
+);
+
+export default ServicesTabs;
